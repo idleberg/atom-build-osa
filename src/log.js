212f82f2f0b52166ad7de0bbfc5213116a83b690
idleberg <git@idleberg.com>
2021-04-20 22:09:31 +0200

    add logging module

---
+++ src/log.js
@@ -0,0 +1,7 @@
+import meta from '../package.json';
+import DeveloperConsole from '@atxm/developer-console';
+
+export default new DeveloperConsole({
+  name: meta.name,
+  backgroundColor: 'rosybrown'
+});
